18b1efba26682c36ad4fa69f1c07d3ff5987fa8c
sanju <sanjutl19@gmail.com>
2024-08-29 23:26:44 +0530

    eleventh commit

--- script.js
+++ script.js
@@ -67,3 +67,25 @@ carouselWrapper.addEventListener('touchend', (e) => {
     showPrevItem();
   }
 });
+
+
+let allDiv=document.querySelectorAll('.con2-imgdiv')
+console.log(allDiv)
+let divOption={
+  root:null,
+  rootMargin:'0px',
+  threshold:0.5,
+}
+let divObserver=new IntersectionObserver((entries)=>{
+  entries.forEach((entry)=>{
+    if(!entry.isIntersecting){
+      return
+    }
+      entry.target.classList.add('fade')
+    
+  })
+},divOption)
+
+allDiv.forEach((div)=>{
+  divObserver.observe(div)
+})
