c05c809fe108c4ee315c778f1ab1d3a3091086db
unnikuttan t l <sanjutl19@gmail.com>
2024-11-07 00:12:42 +0530

    final commit

--- pages/modular.js
+++ pages/modular.js
@@ -21,7 +21,6 @@ article.forEach((article)=>{
 slides[index].classList.add('active')
 article[index].classList.add('active')
 slidewidth=slides[0].getBoundingClientRect().width
-console.log(slidewidth);
 
     wrapper.style.transform=`translate3d(-${index * slidewidth+margin}px,0px, 0px)`
     index=(index+1)%slides.length;
@@ -30,3 +29,33 @@ console.log(slidewidth);
     }
 }
 let intervalSliding=setInterval(updateImg,3000)
+
+
+
+const wrapper2=document.getElementById('carousel-2')
+const bullet=document.querySelector('.bullets')
+const bulletChild=Array.from(bullet.children)
+
+const slide2=Array.from(wrapper2.children)
+console.log(slide2);
+let index2=0;
+function updateCarousle(){
+    slide2.forEach((slide)=>{
+        slide.classList.remove('active')
+    })
+    bulletChild.forEach((bullet)=>{
+        bullet.classList.remove('active')
+    })
+    
+    slide2[index2].classList.add('active')
+    bulletChild[index2].classList.add('active')
+    slideWidth=slide2[index2].getBoundingClientRect().width
+    
+    wrapper2.style.transform=`translateX(-${index2*slideWidth}px)`
+
+    index2=(index2+1)%slide2.length;
+    console.log(index2);
+    
+    
+}
+let sliding=setInterval(updateCarousle,3000)
